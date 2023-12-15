06dac74f995b09bc6365cfd20ea4df30d096468d
dhirajj23 <dhiraj.jadhav@esehat.org>
2023-12-15 10:49:22 +0530

    data

--- controllers/candidate.py
+++ controllers/candidate.py
@@ -9,13 +9,25 @@ import base64
 class GPCandidatePortal(CustomerPortal):
     
     @http.route(['/my/gpexam/list'],type="http",auth="user",website=True)
-    def GPExamListView(self,**kw):
-        partner_id = request.env.user.partner_id.id
-        registered_exams = request.env["survey.user_input"].sudo().search([('partner_id','=',partner_id)])
-        # import wdb; wdb.set_trace(); 
-        vals = {"registered_exams":registered_exams}
-        return request.render("bes.gp_exam_list_view", vals)
-    
+    def GPExamListView(self,**kwargs):
+        parameter_value = kwargs.get('gpexamcand')
+        print(parameter_value)
+
+        if parameter_value:
+            partner_id = request.env.user.partner_id.id
+            registered_exams = request.env["gp.exam.schedule"].sudo().search([])
+            print('registered_examsssssssssssssssssssssssssssss',registered_exams)
+            # import wdb; wdb.set_trace(); 
+            vals = {"registered_exams":registered_exams}
+            return request.render("bes.gp_exam_candidate", vals)
+        else:
+
+            partner_id = request.env.user.partner_id.id
+            registered_exams = request.env["survey.user_input"].sudo().search([('partner_id','=',partner_id)])
+            # import wdb; wdb.set_trace(); 
+            vals = {"registered_exams":registered_exams}
+            return request.render("bes.gp_exam_list_view", vals)
+        
     
     
     # @http.route(['/my/gpexam/startexam'],type="http",auth="user",website=True)
