34c6eb6b34a9f323815bbd84d9ee781660b53c27
saif-esehat <saif.kazi@esehat.org>
2024-05-03 02:10:00 +0530

    data

--- controllers/institute.py
+++ controllers/institute.py
@@ -253,9 +253,20 @@ class InstitutePortal(CustomerPortal):
         state_data = [{'id': state.id, 'name': state.name} for state in states]
         return json.dumps(state_data)
 
+    @http.route(['/my/updatetransaction'],method=["POST"], type="http", auth="user", website=True)
+    def UpdateTransaction(self, **kw):
+        import wdb; wdb.set_trace();
+        
+        invoice_id = kw.get('invoice_id')
+        
+        return request.redirect("/my/invoices/"+str(invoice_id))
+
     @http.route(['/my/creategpinvoice'],method=["POST"], type="http", auth="user", website=True)
     def CreateGPinvoice(self, **kw):
         # import wdb; wdb.set_trace();
+        
+        
+        
 
         
         user_id = request.env.user.id
@@ -264,12 +275,13 @@ class InstitutePortal(CustomerPortal):
         institute_id = request.env["bes.institute"].sudo().search(
             [('user_id', '=', user_id)])
         
-        transaction_id = kw.get('transaction_id')
-        bank_name = kw.get('bank_name')
-        total_amount = int(kw.get('total_amount'))
-        file_content = kw.get("transaction_slip").read()
         
-        filename = kw.get('transaction_slip').filename
+        # transaction_id = kw.get('transaction_id')
+        # bank_name = kw.get('bank_name')
+        # total_amount = int(kw.get('total_amount'))
+        # file_content = kw.get("transaction_slip").read()
+        
+        # filename = kw.get('transaction_slip').filename
  
 
         
@@ -295,10 +307,10 @@ class InstitutePortal(CustomerPortal):
             'gp_batch_ok':True,
             'batch':batch.id,
             'l10n_in_gst_treatment':'unregistered',
-            'transaction_id': transaction_id,
-            'bank_name': bank_name,
-            'total_amount':total_amount,
-            'transaction_slip': base64.b64encode(file_content)
+            # 'transaction_id': transaction_id,
+            # 'bank_name': bank_name,
+            # 'total_amount':total_amount,
+            # 'transaction_slip': base64.b64encode(file_content)
             
             # Add other invoice fields as needed
         }
