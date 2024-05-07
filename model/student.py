41d5be223b3ea0a5d3ba494f5fd4406bbe5961bd
Lokesh Esehat <lokesh.dalvi@esehat.org>
2024-05-07 14:30:25 +0000

    save

--- model/student.py
+++ model/student.py
@@ -64,12 +64,12 @@ class GPCandidate(models.Model):
     attendance_compliance_1 = fields.Selection([
         ('yes', 'Yes'),
         ('no', 'No')
-    ],string="Whether Attendance record of the candidate comply with DGS Guidelines 1 of 2018 as per para 3.2 for GP / 7 of 2010 as per para 3.3 for CCMC (YES/ NO)", default='no',tracking=True)
+    ],string="Whether Attendance record of the candidate comply with DGS training circular 1 of 2018 as per para 3.2 for GP / 7 of 2010 as per para 3.3 for CCMC (YES/ NO)", default='no',tracking=True)
     
     attendance_compliance_2 = fields.Selection([
          ('yes', 'Yes'),
          ('no', 'No')
-    ], string="Attendance record of the candidate not comply with DGS Guidelines 1 of 2018 as per para 3.2 for GP / 7 of 2010 as per para 3.3 for CCMC and whether same has been informed to the DGS (YES/ NO)", default='no',tracking=True)
+    ], string="Attendance record of the candidate not comply with DGS training circular 1 of 2018 as per para 3.2 for GP / 7 of 2010 as per para 3.3 for CCMC and whether same has been informed to the DGS (YES/ NO)", default='no',tracking=True)
 
     stcw_certificate = fields.One2many("gp.candidate.stcw.certificate","candidate_id",string="STCW Certificate",tracking=True)
     
@@ -576,12 +576,12 @@ class CCMCCandidate(models.Model):
     attendance_compliance_1 = fields.Selection([
         ('yes', 'Yes'),
         ('no', 'No')
-    ],string="Whether Attendance record of the candidate comply with DGS Guidelines 1 of 2018 as per para 3.2 for GP / 7 of 2010 as per para 3.3 for CCMC (YES/ NO)", default='no',tracking=True)
+    ],string="Whether Attendance record of the candidate comply with DGS training circular 1 of 2018 as per para 3.2 for GP / 7 of 2010 as per para 3.3 for CCMC (YES/ NO)", default='no',tracking=True)
     
     attendance_compliance_2 = fields.Selection([
          ('yes', 'Yes'),
          ('no', 'No')
-    ], string="Attendance record of the candidate not comply with DGS Guidelines 1 of 2018 as per para 3.2 for GP / 7 of 2010 as per para 3.3 for CCMC and whether same has been informed to the DGS (YES/ NO)", default='no',tracking=True)
+    ], string="Attendance record of the candidate not comply with DGS training circular 1 of 2018 as per para 3.2 for GP / 7 of 2010 as per para 3.3 for CCMC and whether same has been informed to the DGS (YES/ NO)", default='no',tracking=True)
 
     stcw_certificate = fields.One2many("ccmc.candidate.stcw.certificate","candidate_id",string="STCW Certificate",tracking=True)
     
