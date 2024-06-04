785e64c599d3f9cd78d9e9ede8668d6ce347dd30
farkankhud <sharique.ali2001@yahoo.com>
2024-06-04 16:53:18 +0530

    Data

--- static/js/examiner_portal_marksheet.js
+++ static/js/examiner_portal_marksheet.js
@@ -94,6 +94,52 @@ odoo.define('bes.ExaminerPortalMarksheet', function (require) {
         }
     )
 
+    publicWidget.registry.ExaminerPortalCCMCGSKOral = publicWidget.Widget.extend(
+        {
+            selector : ".confirm_ccmc_gsk_oral_marksheet_class",
+            events:{
+                'click':"_onConfirmCCMCGSKOral"
+            },
+            _onConfirmCCMCGSKOral: function(evt){
+                
+                debugger
+
+                var gsk_oral_marksheet_id = evt.target.id
+
+                var postData = {
+                    id: mek_marksheet_id // Assuming you want to pass the ID in the request body
+                };
+                var result = [];
+
+
+              
+                
+    
+                // $.ajax({
+                //     type: "POST",
+                //     url: '/confirm/mek/marksheet',
+                //     data: JSON.stringify(postData) ,
+                //     contentType: 'application/json',                    
+                //     success: function (response) {
+                //         // debugger
+                //         console.log("POST request successful:", response);
+                //         location.reload();
+
+                //         // if response["status"]
+                //         // Handle success response
+                //     },
+                //     error: function (xhr, status, error) {
+                //         // debugger
+                //         console.error("POST request failed:", error);
+                        
+                //         // Handle error
+                //     }
+                // });
+
+            }
+        }
+    )
+
 
 
 });
