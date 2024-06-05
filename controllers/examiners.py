135c1f1ff250c79ad2ad4103e52fc2bc140ce05d
Lokesh Esehat <lokesh.dalvi@esehat.org>
2024-06-05 06:45:54 +0000

    save

--- controllers/examiners.py
+++ controllers/examiners.py
@@ -1933,7 +1933,10 @@ class ExaminerPortal(CustomerPortal):
         examiner = request.env['bes.examiner'].sudo().search([('user_id','=',user_id)])
         examiner_assignments = request.env['exam.type.oral.practical.examiners'].sudo().search([('dgs_batch.id','=',batch_id),('examiner','=',examiner.id)])
         # marksheets = request.env['exam.type.oral.practical.examiners.marksheet'].sudo().search([('examiners_id','=',assignment_id)])
-        
+        examiner_assignments.write({
+            'marksheet_uploaded' : True,
+            'extended' : True 
+        })
             
         return request.redirect("/my/assignments/batches/candidates/"+str(batch_id)+'/'+str(assignment_id))
         # return request.render("bes.examiner_assignment_candidate_list")
@@ -1986,7 +1989,10 @@ class ExaminerPortal(CustomerPortal):
         examiner = request.env['bes.examiner'].sudo().search([('user_id','=',user_id)])
         examiner_assignments = request.env['exam.type.oral.practical.examiners'].sudo().search([('dgs_batch.id','=',batch_id),('examiner','=',examiner.id)])
         # marksheets = request.env['exam.type.oral.practical.examiners.marksheet'].sudo().search([('examiners_id','=',assignment_id)])
-        
+        examiner_assignments.write({
+            'marksheet_uploaded' : True,
+            'extended' : True 
+        })
             
         return request.redirect("/my/assignments/batches/candidates/"+str(batch_id)+'/'+str(assignment_id))
         # return request.render("bes.examiner_assignment_candidate_list")
