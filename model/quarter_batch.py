bf1b8309b36caeefb53e7f04bbc4d500a5406087
farkankhud <sharique.ali2001@yahoo.com>
2024-06-26 16:47:54 +0530

    data

--- model/quarter_batch.py
+++ model/quarter_batch.py
@@ -1,6 +1,10 @@
 from odoo import api, fields, models
 from odoo.exceptions import UserError,ValidationError
 from datetime import datetime
+from collections import defaultdict
+import matplotlib.pyplot as plt
+import io
+import base64
 
 
 
@@ -39,6 +43,96 @@ class DGSBatch(models.Model):
     gp_url = fields.Char('URL for GP candidates',compute="_compute_url")
     ccmc_url = fields.Char('URL for ccmc candidates',compute="_compute_ccmc_url")
     form_deadline = fields.Date(string="Registration Form Dead Line",tracking=True)
+    
+    plot_image = fields.Binary(string='Pass Percentage Plot')
+
+    
+    
+    
+    def get_pass_percentage(self,exams=None):
+        if exams == None:
+            exams = self.env['gp.exam.schedule'].sudo().search([('dgs_batch','=',self.id),('attempt_number','=','1')])
+        total_counts = defaultdict(int)
+        pass_counts = defaultdict(int)
+        # exams = self.env['gp.exam.schedule'].sudo().search([('dgs_batch','=',self.id),('attempt_number','=','1')])
+
+        
+        candidates = []
+        
+        for record in exams:
+            candidates.append({'name': record.gp_candidate.name, 'institute': record.gp_candidate.institute_id.name ,'institute_code': record.gp_candidate.institute_id.code, 'status': record.certificate_criteria},)
+        
+        
+        unique_institutes = {(item['institute'], item['institute_code']) for item in candidates}
+
+        # Convert the set to a list of dictionaries for better readability
+        unique_institutes_list = [{'institute': ins, 'institute_code': code} for ins, code in unique_institutes]
+
+        labels = set((d['institute_code'], d['institute']) for d in candidates)
+
+        
+        for entry in candidates:
+            institute = entry['institute_code']
+            total_counts[institute] += 1
+            if entry['status'] == 'passed':
+                pass_counts[institute] += 1
+
+        pass_percentages = {institute: (pass_counts[institute] / total) * 100 for institute, total in total_counts.items()}
+        # import wdb; wdb.set_trace();
+        # print(pass_percentages)
+        
+        institutes = list(pass_percentages.keys())
+        percentages = list(pass_percentages.values())
+
+        plt.figure(figsize=(10, 6))
+        bars = plt.bar(institutes, percentages, color='skyblue',width=0.5)
+        plt.xlabel('Institutes')
+        plt.ylabel('Pass Percentage')
+        plt.title('Pass Percentage of Students Institute-wise')
+        plt.xticks(rotation=45,fontsize=7)
+        plt.ylim(0, 110)
+        
+  
+
+        
+        
+        plt.legend()
+        
+        # for ins in unique_institutes_list:
+        #     institute = ins['institute']
+        #     institute_code = ins['institute_code']
+        #     plt.legend(loc='lower left', bbox_to_anchor=(0.5, -0.2), ncol=2,title=institute_code+" - "+institute)
+        
+        # for i, (ins_code, institute) in enumerate(labels):
+        #     plt.text(i - 0.2, -0.6, f'{ins_code:10}         {institute}', fontsize=10, va='top')
+
+
+
+        
+
+        
+        for bar in bars:
+            yval = bar.get_height()
+            plt.text(bar.get_x() + bar.get_width()/2, yval + 1, f'{round(yval, 2)}%', 
+                         ha='center', va='bottom', fontsize=6, rotation=70)
+            plt.plot([bar.get_x() + bar.get_width()/2, bar.get_x() + bar.get_width()/2], 
+                         [yval, yval + 1], color='black', linewidth=0.5)
+        
+        
+
+        plt.tight_layout()
+        # Save the plot to an in-memory buffer
+        buf = io.BytesIO()
+        plt.savefig(buf, format='png',bbox_inches='tight')
+        buf.seek(0)
+        image_base64 = base64.b64encode(buf.read()).decode('utf-8')
+
+        # Store the image in the record
+        self.write({'plot_image': image_base64}) 
+        
+        
+        # return pass_percentages
+    
 
     @api.depends('repeater_batch')
     def _compute_url(self):
@@ -62,8 +156,12 @@ class DGSBatch(models.Model):
 
     def move_confirm(self):
         exams = self.env['gp.exam.schedule'].search([('dgs_batch','=',self.id)])
+        
         for exam in exams:
             exam.move_done()
+            
+        percentages = self.get_pass_percentage(exams)
+        import wdb; wdb.set_trace();
         self.state = '2-confirmed'
         
     def move_dgs_approved(self):
@@ -147,6 +245,11 @@ class DGSBatchReport(models.AbstractModel):
     _inherit = ['mail.thread','mail.activity.mixin']
     _description = "DGS Batch Report"
     
+    
+    
+
+    
+    
     @api.model
     def _get_report_values(self, docids, data=None):
         
@@ -159,6 +262,7 @@ class DGSBatchReport(models.AbstractModel):
 
         if report_type == 'Fresh' and course == 'GP':
             exams = self.env['gp.exam.schedule'].sudo().search([('dgs_batch','=',docs1.id),('attempt_number','=','1')])
+            self.get_pass_percentage(exams)
         elif report_type == 'Repeater' and course == 'GP':
             exams = self.env['gp.exam.schedule'].sudo().search([('dgs_batch','=',docs1.id),('attempt_number','>','1')])
 
