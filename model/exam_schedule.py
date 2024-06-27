595bfb99d95c68c2a8a7711694b502bcae0f4ba5
Lokesh Esehat <lokesh.dalvi@esehat.org>
2024-06-27 07:05:39 +0000

    save

--- model/exam_schedule.py
+++ model/exam_schedule.py
@@ -1833,7 +1833,7 @@ class ExamOralPracticalExaminers(models.Model):
                 if record.exam_type == 'practical_oral':
                     count = 0
                     for sheet in record.marksheets:
-                       if sheet.mek_oral.gsk_oral_draft_confirm == 'confirm' and sheet.gsk_prac.gsk_practical_draft_confirm == 'confirm':
+                       if sheet.gsk_oral.gsk_oral_draft_confirm == 'confirm' and sheet.gsk_prac.gsk_practical_draft_confirm == 'confirm':
                            count += 1
                     record.candidate_done = count
                 else:
