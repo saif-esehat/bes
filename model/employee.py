0c6f03b7d028e62a4bd59ff4bef7af02f7108094
saif.kazi@esehat.org <saif-esehat>
2024-07-25 14:37:36 +0000

    Data

--- model/employee.py
+++ model/employee.py
@@ -9,12 +9,6 @@ class EmployeeInherited(models.Model):
     _inherit = 'hr.employee'
 
     examiner = fields.Boolean("Examiner")
-<<<<<<< HEAD
     institute_id = fields.Many2many("bes.institute",'institute_id',string="Institute")
     expense_sheet = fields.Many2one('hr.expense.sheet','Expense')
     time_sheet = fields.Many2one('time.sheet.report','Time Sheet')
-=======
-    # institute_id = fields.Many2many("bes.institute",'institute_id',string="Institute")
-    # expense_sheet = fields.Many2one('hr.expense.sheet','Expense')
-    # time_sheet = fields.Many2one('time.sheet.report','Time Sheet')
->>>>>>> d739911 (datA)
