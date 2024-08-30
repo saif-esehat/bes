529d9a37379e888c27af93a0d2e3be1518126194
saif-esehat <saif.kazi@esehat.org>
2024-08-30 15:19:54 +0530

    DATA

--- controllers/examiners.py
+++ controllers/examiners.py
@@ -183,18 +183,19 @@ class ExaminerPortal(CustomerPortal):
     
     @http.route(['/confirm/gsk/marksheet'],method=["POST"],type="json", auth="user")
     def ConfirmGSKMarksheet(self, **kw):
+     
         print("KW Confirm GSK")
         print(request.jsonrequest)
         data = request.jsonrequest
         marksheet_id = data["id"]
         last_part = marksheet_id.split('_')[-1]
         marksheet_id = int(last_part)
-
+        
         marksheet = request.env["exam.type.oral.practical.examiners.marksheet"].sudo().search([('id','=',marksheet_id)])
         marksheet.gsk_oral.write({"gsk_oral_draft_confirm": 'confirm' })
         marksheet.gsk_prac.write({"gsk_practical_draft_confirm": 'confirm' })
+        marksheet.gp_marksheet.write({'gsk_oral_prac_attendance': data['attendance_id']})
 
-        # import wdb; wdb.set_trace()
         marksheet.examiners_id.compute_candidates_done()
         marksheet.examiners_id.check_absent()
         return json.dumps({"status":"success"})
@@ -224,9 +225,12 @@ class ExaminerPortal(CustomerPortal):
     def ConfirmMEKMarksheet(self, **kw):
         print("KW Confirm MEK")
         
+        # import wdb; wdb.set_trace()
+        
         print(request.jsonrequest)
         data = request.jsonrequest
         marksheet_id = data["id"]
+        
 # Split the string by underscore and take the last element
         last_part = marksheet_id.split('_')[-1]
 
@@ -237,6 +241,8 @@ class ExaminerPortal(CustomerPortal):
         marksheet = request.env["exam.type.oral.practical.examiners.marksheet"].sudo().search([('id','=',marksheet_id)])
         marksheet.mek_oral.write({"mek_oral_draft_confirm": 'confirm' })
         marksheet.mek_prac.write({"mek_practical_draft_confirm": 'confirm' })
+        marksheet.gp_marksheet.write({'mek_oral_prac_attendance': data['attendance_id']})
+
         marksheet.examiners_id.compute_candidates_done()
         marksheet.examiners_id.check_absent()
         return json.dumps({"status":"success"})
