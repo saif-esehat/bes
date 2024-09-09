10e52af04b2c3ce818f37e1f3e3104280afa9794
Lokesh Esehat <lokesh.dalvi@esehat.org>
2024-09-09 10:41:04 +0000

    save

--- static/js/examiner_portal_marksheet.js
+++ static/js/examiner_portal_marksheet.js
@@ -49,7 +49,7 @@ odoo.define('bes.ExaminerPortalMarksheet', function (require) {
                 // Check if attendance is marked as 'present' but oral or practical marks are not present
                 if (attendance_element.value === 'present' && (parseInt(gsk_oral_marks) == 0 || parseInt(gsk_practical_marks) == 0)) {
                     confirm("Are you sure you want to mark this  candidate present as their marks are 0?");   
-                    return;
+                    // return;
                 }
 
 
@@ -142,7 +142,7 @@ odoo.define('bes.ExaminerPortalMarksheet', function (require) {
                 // Check if attendance is marked as 'present' but oral or practical marks are not present
                 if (attendance_element.value === 'present' && (parseInt(mek_oral_marks) == 0 || parseInt(mek_practical_marks) == 0)) {
                     confirm("Are you sure you want to mark this  candidate present as their marks are 0?");   
-                    return;
+                    // return;
                 }
 
                 debugger;
