1452ff1a736915d01b66edb623e166b41bdc5f29
saif.kazi@esehat.org <saif-esehat>
2024-10-04 10:02:45 +0000

    datA

--- model/__init__.py
+++ model/__init__.py
@@ -1,39 +1,3 @@
-<<<<<<< HEAD
-from . import course_master
-from . import institute
-from . import survey
-from . import batches
-from . import examiner
-from . import examiner_expenses
-from . import student
-from . import exam_center
-from . import invoice
-from . import exam_schedule
-from . import admit_card
-from . import expense
-from . import batches_report
-from .marksheets import mek
-from .marksheets import gsk
-from . import quarter_batch
-# from . import employee
-from . import attendance_sheet
-from . import time_sheet
-from . import examination_report
-from . import candidate_approval
-from . import sep_batches
-from . import sep_candidate
-from . import expense_report
-from .iv_exam import iv_batches
-from .iv_exam import iv_candidates
-from .iv_exam import candidates_application
-from .iv_exam import iv_candidate_admit_card
-from .iv_exam import iv_attendance_sheet
-from .iv_exam import iv_oral_attendance_sheet
-from .iv_exam import written_examination
-
-
-
-=======
 from . import course_master
 from . import institute
 from . import survey
@@ -68,4 +32,3 @@ from .iv_exam import written_examination
 
 
 
->>>>>>> a0fb074e01eea6c91c986d52db2a81bdbac97976
