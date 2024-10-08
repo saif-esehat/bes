51c5893d10b5bd7a51e9d396b5abf699fbcdcdfb
ankitkambl <ankit.kamble@esehat.org>
2024-10-08 08:10:23 +0000

    ADD

--- model/__init__.py
+++ model/__init__.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from . import course_master
 from . import institute
 from . import survey
@@ -34,38 +33,3 @@ from .ship_visits import ccmc_ship_visit
 
 
 
-=======
-from . import course_master
-from . import institute
-from . import survey
-from . import batches
-from . import examiner
-from . import examiner_expenses
-from . import student
-from . import exam_center
-from . import invoice
-from . import exam_schedule
-from . import admit_card
-from . import expense
-from . import batches_report
-from .marksheets import mek
-from .marksheets import gsk
-from . import quarter_batch
-# from . import employee
-from . import attendance_sheet
-from . import time_sheet
-from . import examination_report
-from . import candidate_approval
-from . import sep_batches
-from . import sep_candidate
-from . import expense_report
-from .iv_exam import iv_batches
-from .iv_exam import iv_candidates
-from .iv_exam import candidates_application
-from .iv_exam import iv_candidate_admit_card
-from .iv_exam import iv_attendance_sheet
-from .iv_exam import iv_oral_attendance_sheet
-from .iv_exam import written_examination
-
-
->>>>>>> 2953362cf2eeacac0abdde1a66198eb91319058f
