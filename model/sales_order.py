24cb997b0ca1e62e5b76be624355e13a0503feee
Usman-Shaikhnag <ushaikhnag@gmail.com>
2024-11-19 13:49:44 +0530

    books

--- model/sales_order.py
+++ model/sales_order.py
@@ -10,10 +10,14 @@ class SalesOrderInherited(models.Model):
 
     payment_slip = fields.Binary("Payment Slip")
     slip_file_name = fields.Char('filename')
-    
-class SalesOrderInherited(models.Model):
-    _inherit = 'sale.order.line'
 
     tracking_id = fields.Char('Tracking Id')
     transaction_id = fields.Char("Transaction Id")
     sequence_id = fields.Char("Sequence")
+    
+# class SalesOrderInherited(models.Model):
+#     _inherit = 'sale.order.line'
+
+#     tracking_id = fields.Char('Tracking Id')
+#     transaction_id = fields.Char("Transaction Id")
+#     sequence_id = fields.Char("Sequence")
